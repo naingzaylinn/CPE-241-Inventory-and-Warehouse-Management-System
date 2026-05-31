86c95edbd80a294706fdd31a4b39b06efcee9bfe
MoeD1234 <mhaung84@gmail.com>
2026-05-31 22:10:33 +0700

    Create suppliers.api.js

---
+++ warehouse-app/client/src/api/suppliers.api.js
@@ -0,0 +1,9 @@
+import { http } from './http'
+
+export const suppliersApi = {
+  list: () => http.get('/suppliers'),
+  get: (id) => http.get(`/suppliers/${id}`),
+  create: (data) => http.post('/suppliers', data),
+  update: (id, data) => http.put(`/suppliers/${id}`, data),
+  delete: (id) => http.delete(`/suppliers/${id}`)
+}
